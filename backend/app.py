cad45393d65f1f1ddff31a93de3e90df9dc15887
Tejaswini050302 <tejaswini2.mitblr2024@learner.manipal.edu>
2026-04-12 00:47:22 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -51,4 +51,4 @@ def group():
 
 # IMPORTANT FOR DEPLOYMENT
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7000)))
